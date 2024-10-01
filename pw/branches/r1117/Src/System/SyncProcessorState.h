2bdfee33ad7b37245b7bc6f15317f591572861be
SSE4 <tomskside@gmail.com>
2024-10-01 02:45:50 -0700

    use _controlfp_s instead of asm fstcw/fldcw to save/load FPU control word

--- pw/branches/r1117/Src/System/SyncProcessorState.h
+++ pw/branches/r1117/Src/System/SyncProcessorState.h
@@ -1,6 +1,7 @@
 #ifndef _SYNC_PROCESSOR_STATE_H_
 #define _SYNC_PROCESSOR_STATE_H_
 
+// FIXME : might be not suitable for x64, precision control is not supported
 static const DWORD LOGIC_PROCESSOR_STATE = _EM_INVALID | _EM_ZERODIVIDE | _EM_OVERFLOW | _EM_UNDERFLOW | _EM_INEXACT | _EM_DENORMAL | _PC_24	| _RC_NEAR;
 static const DWORD UI_PROCESSOR_STATE = _EM_INVALID | _EM_ZERODIVIDE | _EM_OVERFLOW | _EM_UNDERFLOW | _EM_INEXACT | _EM_DENORMAL | _PC_64	| _RC_NEAR;
 
@@ -15,21 +16,14 @@ bool IsProcessorStateForUI();
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #define NI_SYNC_FPU_START   \
-  WORD nFPUStatus;          \
-  __asm                     \
-  {                         \
-    __asm fstcw nFPUStatus  \
-    __asm wait              \
-  }                         \
+  unsigned nFPUStatus;          \
+  _controlfp_s(&nFPUStatus, 0, 0); \
   SyncProcessorState();
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #define NI_SYNC_FPU_END     \
-  __asm                     \
-  {                         \
-    __asm fldcw nFPUStatus  \
-    __asm wait              \
-  }
+  unsigned unused;          \
+  _controlfp_s(&unused, nFPUStatus, _MCW_EM | _MCW_RC | _MCW_PC);
 
 #endif  // !defined( NI_PLATF_LINUX )
 
